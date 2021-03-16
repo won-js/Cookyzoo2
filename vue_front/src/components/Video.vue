b5405b412045c47615e57c48716aa25ce9b60467
엄재식 <skokk33@gmail.com>
2021-03-16 10:21:58 +0900

    cook-sample, modal CSS 수정

--- vue_front/src/components/Video.vue
+++ vue_front/src/components/Video.vue
@@ -1,243 +1,247 @@
 <template>
-  <div id="container">
-    <video id="vid" :src="this.videoSource" autoplay />
-		{{this.videoSource}}
-  </div>
+	<div id="container">
+		<video id="vid" :src="this.videoSource" autoplay />
+	</div>
 </template>
 
 <script>
 import * as THREE from "three";
 
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { mapGetters, mapMutations } from "vuex";
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import {mapGetters, mapMutations} from "vuex";
 
 export default {
-  data() {
-    return {
-      camera: undefined,
-      scene: undefined,
-      renderer: undefined,
-      controls: undefined,
-      loader: undefined,
-      mixer: undefined,
-      clock: new THREE.Clock(),
-      videoSource: undefined,
-      model: undefined,
-      followAudio: new Audio(),
-      motions: [],
-      animations: {},
-      curStep: undefined,
-      animal: 0,
-      startCheck: false,
-    };
-  },
-  computed: {
-    ...mapGetters({
-      getContents: "game/contents",
-      getStep: "game/step",
-      getSuccess: "game/success",
-      getAnimalAnimation: "game/animalAnimation",
-      getStart: "game/start",
-      getLessonName: "lesson/name",
-      getLessonId: "lesson/lessonId",
-    }),
-    getVideo() {
-      return `videos/lesson_${this.getLessonId}/${
-        this.getContents[this.getStep].video
-      }`;
-    },
-  },
-  watch: {
-    getStep() {
-      this.videoSource = this.getVideo;
-      this.mixer = new THREE.AnimationMixer(this.model);
-      const action = this.mixer.clipAction(this.animations.explain);
+	data() {
+		return {
+			camera: undefined,
+			scene: undefined,
+			renderer: undefined,
+			controls: undefined,
+			loader: undefined,
+			mixer: undefined,
+			clock: new THREE.Clock(),
+			videoSource: undefined,
+			model: undefined,
+			followAudio: new Audio(),
+			motions: [],
+			animations: {},
+			curStep: undefined,
+			animal: 0,
+			startCheck: false,
+		};
+	},
+	computed: {
+		...mapGetters({
+			getContents: "game/contents",
+			getStep: "game/step",
+			getSuccess: "game/success",
+			getAnimalAnimation: "game/animalAnimation",
+			getStart: "game/start",
+			getLessonName: "lesson/name",
+			getLessonId: "lesson/lessonId",
+		}),
+		getVideo() {
+			return `videos/lesson_${this.getLessonId}/${
+				this.getContents[this.getStep].video
+			}`;
+		},
+	},
+	watch: {
+		getStep() {
+			this.videoSource = this.getVideo;
+			this.mixer = new THREE.AnimationMixer(this.model);
+			const action = this.mixer.clipAction(this.animations.explain);
 
-      action.play();
-      this.scene.add(this.model);
-      this.change = false;
-    },
-    getSuccess() {
-      if (this.getSuccess) {
-        this.mixer = new THREE.AnimationMixer(this.model);
-        this.mixer.clipAction(this.animations.clap).play();
-        this.scene.add(this.model);
-        this.setSuccess(false);
+			action.play();
+			this.scene.add(this.model);
+			this.change = false;
+		},
+		getSuccess() {
+			if (this.getSuccess) {
+				this.mixer = new THREE.AnimationMixer(this.model);
+				this.mixer.clipAction(this.animations.clap).play();
+				this.scene.add(this.model);
+				this.setSuccess(false);
 
-        // setTimeout(() => {
-        //   this.setStep(this.getStep + 1);
-        // },3000)
-      }
-    },
-    getStart() {
-      if (this.getStart) {
-        this.init();
-        this.animate();
-        this.followAudio.src = "./audio/followMe.wav";
-        this.videoSource = this.getVideo;
-      }
-    },
-  },
-  methods: {
-    ...mapMutations({
-      setSuccess: "game/SUCCESS_UPDATED",
-      setStep: "game/STEP_UPDATED",
-      setStart: "game/START_UPDATED",
-    }),
-    init() {
-      const container = document.getElementById("container");
+				// setTimeout(() => {
+				//   this.setStep(this.getStep + 1);
+				// },3000)
+			}
+		},
+		getStart() {
+			if (this.getStart) {
+				this.init();
+				this.animate();
+				this.followAudio.src = "./audio/followMe.wav";
+				this.videoSource = this.getVideo;
+			}
+		},
+	},
+	methods: {
+		...mapMutations({
+			setSuccess: "game/SUCCESS_UPDATED",
+			setStep: "game/STEP_UPDATED",
+			setStart: "game/START_UPDATED",
+		}),
+		init() {
+			const container = document.getElementById("container");
 
-      // // camera
-      this.camera = new THREE.PerspectiveCamera(
-        45,
-        window.innerWidth / window.innerHeight,
-        1,
-        2000
-      );
-      this.camera.position.set(250, 150, 300);
+			// // camera
+			this.camera = new THREE.PerspectiveCamera(
+				45,
+				window.innerWidth / window.innerHeight,
+				1,
+				2000
+			);
+			this.camera.position.set(250, 150, 300);
 
-      // // scene
-      this.scene = new THREE.Scene();
+			// // scene
+			this.scene = new THREE.Scene();
 
-      // renderer
-      this.renderer = new THREE.WebGLRenderer({ alpha: true }); // 투명하게 할때 alpha 사용
-      this.renderer.setPixelRatio(window.devicePixelRatio);
-      this.renderer.setSize(window.innerWidth, window.innerHeight);
-      this.renderer.shadowMap.enabled = true;
-      container.appendChild(this.renderer.domElement);
+			// renderer
+			this.renderer = new THREE.WebGLRenderer({alpha: true}); // 투명하게 할때 alpha 사용
+			this.renderer.setPixelRatio(window.devicePixelRatio);
+			this.renderer.setSize(window.innerWidth, window.innerHeight);
+			this.renderer.shadowMap.enabled = true;
+			container.appendChild(this.renderer.domElement);
 
-      // light
-      // character light
-      const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+			// light
+			// character light
+			const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
 
-      hemiLight.position.set(0, 200, 0);
-      this.scene.add(hemiLight);
+			hemiLight.position.set(0, 200, 0);
+			this.scene.add(hemiLight);
 
-      const dirLight = new THREE.DirectionalLight(0xffffff);
+			const dirLight = new THREE.DirectionalLight(0xffffff);
 
-      dirLight.position.set(0, 200, 100);
-      dirLight.castShadow = true;
-      dirLight.shadow.camera.top = 180;
-      dirLight.shadow.camera.bottom = -100;
-      dirLight.shadow.camera.left = -120;
-      dirLight.shadow.camera.right = -120;
-      this.scene.add(dirLight);
+			dirLight.position.set(0, 200, 100);
+			dirLight.castShadow = true;
+			dirLight.shadow.camera.top = 180;
+			dirLight.shadow.camera.bottom = -100;
+			dirLight.shadow.camera.left = -120;
+			dirLight.shadow.camera.right = -120;
+			this.scene.add(dirLight);
 
-      // test
-      const ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
+			// test
+			const ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
 
-      this.scene.add(ambientLight);
+			this.scene.add(ambientLight);
 
-      // 밝기 조절
-      const pointLight = new THREE.PointLight(0xffffff, 4);
+			// 밝기 조절
+			const pointLight = new THREE.PointLight(0xffffff, 4);
 
-      this.camera.add(pointLight);
-      this.scene.add(this.camera);
+			this.camera.add(pointLight);
+			this.scene.add(this.camera);
 
-      //gltf
-      this.loader = new GLTFLoader();
-      console.log(this.getAnimalAnimation, "잘 받아와지나?");
-      this.loader.load(
-        `./fbx/${this.getAnimalAnimation}`, // todo: 여기를 동적으로 변경
-        (gltf) => {
-          this.model = gltf.scene;
-          this.mixer = new THREE.AnimationMixer(this.model);
+			//gltf
+			this.loader = new GLTFLoader();
+			console.log(this.getAnimalAnimation, "잘 받아와지나?");
+			this.loader.load(
+				`./fbx/${this.getAnimalAnimation}`, // todo: 여기를 동적으로 변경
+				gltf => {
+					this.model = gltf.scene;
+					this.mixer = new THREE.AnimationMixer(this.model);
 
-          for (let i = 0; i < gltf.animations.length; i++) {
-            if (gltf.animations[i].name[0] !== "B") {
-              this.motions.push(gltf.animations[i].name);
-              this.animations[gltf.animations[i].name] = gltf.animations[i];
-            }
-          }
-          console.log(this.motions);
+					for (let i = 0; i < gltf.animations.length; i++) {
+						if (gltf.animations[i].name[0] !== "B") {
+							this.motions.push(gltf.animations[i].name);
+							this.animations[gltf.animations[i].name] = gltf.animations[i];
+						}
+					}
+					console.log(this.motions);
 
-          const action = this.mixer.clipAction(this.animations.explain);
+					const action = this.mixer.clipAction(this.animations.explain);
 
-          action.play();
+					action.play();
 
-          this.model.traverse((child) => {
-            if (child.isMesh) {
-              child.castShadow = true;
-              child.receiveShadow = true;
-            }
-          });
+					this.model.traverse(child => {
+						if (child.isMesh) {
+							child.castShadow = true;
+							child.receiveShadow = true;
+						}
+					});
 
-          // 모델의 크기 조정
-          this.model.scale.set(20, 20, 20);
-          this.model.position.set(190, -10, -50);
+					// 모델의 크기 조정
+					this.model.scale.set(20, 20, 20);
+					this.model.position.set(190, -10, -50);
 
-          this.scene.add(this.model);
-        },
-        undefined,
-        (error) => {
-          console.log(error);
-        }
-      );
+					this.scene.add(this.model);
+				},
+				undefined,
+				error => {
+					console.log(error);
+				}
+			);
 
-      // controls // 컨트롤 안해도 될거 같음
-      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-      this.controls.target.set(0, 100, 0);
-      this.controls.update();
+			// controls // 컨트롤 안해도 될거 같음
+			this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+			this.controls.target.set(0, 100, 0);
+			this.controls.update();
 
-      window.addEventListener("resize", this.onWindowResize, false);
-      document
-        .getElementById("vid")
-        .addEventListener("ended", this.followMotion, false);
+			window.addEventListener("resize", this.onWindowResize, false);
+			document
+				.getElementById("vid")
+				.addEventListener("ended", this.followMotion, false);
 
-      this.renderer.setSize(window.innerWidth * 0.8, window.innerHeight);
-    },
-    onWindowResize() {
-      this.camera.aspect = (window.innerWidth * 0.8) / window.innerHeight;
-      this.camera.updateProjectionMatrix();
-      this.renderer.setSize(window.innerWidth * 0.8, window.innerHeight);
-    },
-    animate() {
-      requestAnimationFrame(this.animate);
-      const delta = this.clock.getDelta();
+			this.renderer.setSize(window.innerWidth * 0.8, window.innerHeight);
+		},
+		onWindowResize() {
+			this.camera.aspect = (window.innerWidth * 0.8) / window.innerHeight;
+			this.camera.updateProjectionMatrix();
+			this.renderer.setSize(window.innerWidth * 0.8, window.innerHeight);
+		},
+		animate() {
+			requestAnimationFrame(this.animate);
+			const delta = this.clock.getDelta();
 
-      if (this.mixer) this.mixer.update(delta);
+			if (this.mixer) this.mixer.update(delta);
 
-      this.renderer.render(this.scene, this.camera);
-    },
-    followMotion() {
-      //랜덤화
-      let motion;
+			this.renderer.render(this.scene, this.camera);
+		},
+		followMotion() {
+			//랜덤화
+			let motion;
 
-      while (
-        motion === undefined ||
-        motion === "explain" ||
-        motion === "clap"
-      ) {
-        motion = this.motions[Math.floor(Math.random() * this.motions.length)];
-      }
+			while (
+				motion === undefined ||
+				motion === "explain" ||
+				motion === "clap"
+			) {
+				motion = this.motions[Math.floor(Math.random() * this.motions.length)];
+			}
 
-      this.mixer = new THREE.AnimationMixer(this.model);
-      this.mixer.clipAction(this.animations[motion]).play();
-      this.scene.add(this.model);
+			this.mixer = new THREE.AnimationMixer(this.model);
+			this.mixer.clipAction(this.animations[motion]).play();
+			this.scene.add(this.model);
 
-      // 음성 follow~
-      this.followAudio.play();
-    },
-    clap() {
-      this.mixer = new THREE.AnimationMixer(this.model);
-      this.mixer.clipAction(this.animations.clap).play();
-      this.scene.add(this.model);
-    },
-  },
-  mounted() {
-    this.setStart(false);
-  },
+			// 음성 follow~
+			this.followAudio.play();
+		},
+		clap() {
+			this.mixer = new THREE.AnimationMixer(this.model);
+			this.mixer.clipAction(this.animations.clap).play();
+			this.scene.add(this.model);
+		},
+	},
+	mounted() {
+		// this.setStart(true);
+		console.log(123);
+		this.init();
+		this.animate();
+		this.followAudio.src = "./audio/followMe.wav";
+		this.videoSource = this.getVideo;
+	},
 };
 </script>
 
 <style scoped>
 #vid {
-  position: absolute;
-  width: 100%;
-  height: 100vh;
-  object-fit: cover;
-  background-color: black;
-  z-index: -1;
+	position: absolute;
+	width: 100%;
+	height: 100vh;
+	object-fit: cover;
+	background-color: black;
+	z-index: -1;
 }
 </style>
--- vue_front/src/components/cook-modal/character.vue
+++ vue_front/src/components/cook-modal/character.vue
@@ -1,290 +1,326 @@
 <template>
-  <section>
-    <div class="char-title">
-      <p>같이 요리할 캐릭터를 선택해볼까요?</p>
-    </div>
-    <div class="character-box">
-      <carousel-3d :width="300" :height="600">
-        <slide
-          class="character"
-          v-for="(animal, i) in getAnimals"
-          :index="i"
-          :key="animal.id"
-        >
-          <div class="wrap">
-            <img class="rabbit" :src="animal.image" alt="" />
-            <div class="arrow_box">
-              <p class="speech-text">ㅎㅇ 나는 {{ animal.name }}고, {{ animal.information }}이다 ㅇㅋ?</p>
-            </div>
-          </div>
-          <ul>
-            <li @click="selectAnimal(animal.animation)">
-              선택하기
-              <span></span><span></span><span></span><span></span>
-            </li>
-          </ul>
-        </slide>
-      </carousel-3d>
-    </div>
-  </section>
+	<section>
+		<div class="char-title">
+			<p>같이 요리할 캐릭터를 선택해볼까요?</p>
+		</div>
+		<div class="character-box">
+			<carousel-3d
+				:height="carouselHeight"
+				:width="carouselWidth"
+				:on-slide-change="onSlideChanged"
+			>
+				<slide
+					class="character"
+					v-for="(animal, i) in getAnimals"
+					:index="i"
+					:key="animal.id"
+				>
+					<div class="wrap">
+						<img class="rabbit" :src="animal.image" alt="" />
+						<div class="arrow_box">
+							<p class="speech-text">
+								ㅎㅇ 나는 {{ animal.name }}야, 나는 {{ animal.information }}
+							</p>
+						</div>
+					</div>
+				</slide>
+			</carousel-3d>
+		</div>
+		<ul>
+			<li @click="selectAnimal()">
+				선택하기
+				<span></span><span></span><span></span><span></span>
+			</li>
+		</ul>
+	</section>
 </template>
 
 <script>
-import { mapGetters, mapMutations } from "vuex";
+import {mapGetters, mapMutations} from "vuex";
 
 export default {
-  name: "Character",
-  data() {
-    return {
-      animals: [
-        {
-          id: null,
-          name: null,
-          information: null,
-          image: null,
-          animation: null,
-          unlock: null,
-        },
-      ],
-    };
-  },
-  computed: {
-    ...mapGetters({
-      getAnimalAnimation: "game/animalAnimation",
-      getAnimals: "animal/animals",
-    }),
-  },
-  methods: {
-    ...mapMutations({
-      nextPage: "modal/NEXT_PAGE",
-      setAnimalAnimation: "game/ANIMAL_ANIMATION_UPDATED",
-      setStart: "game/START_UPDATED",
-    }),
-    selectAnimal(animalAnimation) {
-      this.setAnimalAnimation(animalAnimation);
-      this.setStart(true);
-      this.nextPage();
-    },
-  },
-  created() {
-    this.setAnimalAnimation(1);
-  },
-  mounted() {
-    console.log("두번째");
-  },
+	name: "Character",
+	data() {
+		return {
+			animals: [
+				{
+					id: null,
+					name: null,
+					information: null,
+					image: null,
+					animation: null,
+					unlock: null,
+				},
+			],
+			carouselHeight: 0,
+			carouselWidth: 0,
+			index: 0,
+		};
+	},
+	computed: {
+		...mapGetters({
+			getAnimalAnimation: "game/animalAnimation",
+			getAnimals: "animal/animals",
+		}),
+	},
+	methods: {
+		...mapMutations({
+			nextPage: "modal/NEXT_PAGE",
+			setAnimalAnimation: "game/ANIMAL_ANIMATION_UPDATED",
+			setStart: "game/START_UPDATED",
+		}),
+		selectAnimal() {
+			this.setAnimalAnimation(this.getAnimals[this.index].animation);
+			// this.setStart(true);
+			this.nextPage();
+		},
+		handleResize(event) {
+			this.carouselHeight = window.innerHeight * 0.4;
+			this.carouselWidth = window.innerWidth * 0.3;
+		},
+		onSlideChanged(index) {
+			this.index = index;
+		},
+	},
+	created() {
+		// this.setAnimalAnimation(1);
+	},
+	mounted() {
+		console.log("두번째");
+		window.addEventListener("resize", this.handleResize);
+		this.handleResize();
+	},
+	beforeDestroy() {
+		// console.log("beforeDestroy...");
+		window.removeEventListener("resize", this.handleResize);
+	},
 };
 </script>
 
 <style scoped>
 section {
-  margin: auto;
+	margin: auto;
 
-  height: 100%;
-  background-image: url("../../assets/images/bg3.jpg");
-  background-size: 100% 100%;
+	height: 100%;
+	background-image: url("../../assets/images/bg3.jpg");
+	background-size: 100% 100%;
 
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
 
-  /* position: relative; */
+	/* position: relative; */
 }
 
 .char-title {
-  flex: 0.05;
+	flex: 1 1 0;
 }
 
 .char-title p {
-  font-size: 25px;
+	font-size: 4vh;
+	font-family: "Katuri";
+	margin: 0;
+	padding: 3vh;
 }
 
-.char-box {
-  flex: 0.95;
-  display: flex;
-}
-
-.char-box p {
-}
-
-.char-box .box-name {
-  /* 캐릭터 이름 */
-  font-size: 25px;
-}
-
-.char-box .box-info {
-  /* 캐릭터 설명 */
-  font-size: 18px;
+.character-box {
+	flex: 5 5 0;
+	margin: 0;
+	padding: 0;
+	display: flex;
 }
 
 .character {
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  width: 31%;
-  padding: 10px;
+	margin: 0;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: center;
+	width: 31%;
+	padding: 1vh;
 }
 
 .carousel-3d-container {
+	margin: 0;
+	padding: 0;
 	overflow: visible;
 }
 
 .carousel-3d-slider {
+	margin: 0;
+	padding: 0;
 }
 
 .carousel-3d-container .carousel-3d-slider .carousel-3d-slide {
-  background-color: rgba(255, 255, 255, 0);
-  border: none;
-  visibility: visible !important;
-  /* border: 1px solid red; */
+	background-color: rgba(255, 255, 255, 0);
+	border: none;
+	visibility: visible !important;
+	/* border: 1px solid red; */
+	margin: 0;
 	padding: 0;
 	overflow: visible;
 }
 
 .carousel-3d-slide ul li {
-  display: none;
+	display: none;
 }
 
 .current ul li {
-  display: block;
-}
-.wrap {
-  position: relative;
-  margin: 0 auto;
+	margin: 0;
+	padding: 0;
 	display: block;
-  width: 300px;
-  height: 300px;
-  /* border: 1px solid blue; */
+}
+
+.wrap {
+	position: relative;
+	margin: 0;
+	padding: 0;
+	display: block;
+	width: 10vh;
+	height: 30vh;
+	/* border: 1px solid blue; */
 }
 
 .rabbit {
-  position: absolute;
-	width: 70%;
+	position: absolute;
+	width: auto;
+	height: 40vh;
 	left: 50%;
+	top: 30%;
+	margin: 0;
+	padding: 0;
 	transform: translateX(-50%);
 }
 .arrow_box {
-  visibility: hidden;
+	visibility: hidden;
 	/* visibility: visible; */
 	position: absolute;
-  left: 60%;
-	bottom: 60%;
-  width: 350px;
-  height: 150px;
-  background: white;
-  border: 4px solid #bbbbbb;
-  border-radius: 35px;
+	left: -120%;
+	bottom: 76%;
+	width: 40vh;
+	height: 15vh;
+	background: white;
+	border: 0.5vh solid #bbbbbb;
+	border-radius: 2.5vh;
+	margin: 0;
+	padding: 0;
 }
 .speech-text {
-  font-size: 25px;
-  font-weight: 500;
-  margin: 5 15px;
+	font-size: 3vh;
+	font-family: "Bazzi";
+	margin: 3vh;
 	text-align: center;
 }
 
 .arrow_box:after,
 .arrow_box:before {
-  top: 100%;
-  left: 20%;
-  border: solid transparent;
-  content: ""; /* after, before사용 시 필요, */
-  height: 0;
-  width: 0;
-  position: absolute;
-  pointer-events: none;
+	top: 100%;
+	left: 30%;
+	border: solid transparent;
+	content: ""; /* after, before사용 시 필요, */
+	height: 0;
+	width: 0;
+	position: absolute;
+	pointer-events: none;
+	border-bottom-color: #bbbbbb;
+	border-width: 3vh;
+	margin: 0;
+	padding: 0;
 }
 
 .arrow_box:after {
-  border-color: rgba(136, 183, 213, 0);
-  border-top-color: #ffffff;
-  border-width: 30px;
-  margin-left: -30px;
+	border-color: rgba(136, 183, 213, 0);
+	margin: 0;
+	padding: 0;
+	border-top-color: #ffffff;
+	border-width: 3vh;
+	margin-left: -3vh;
 }
 
 .arrow_box:before {
-  border-color: rgba(194, 225, 245, 0);
-  border-top-color: #bbbbbb;
-  border-width: 36px;
-  margin-left: -36px;
+	border-color: rgba(194, 225, 245, 0);
+	border-top-color: #bbbbbb;
+	border-width: 3vh;
+	margin-left: -3vh;
 }
 .current .rabbit:hover + div.arrow_box {
-  visibility: visible;
+	visibility: visible;
 }
 
 p {
-  padding: 10px;
+	padding: 1vh;
 }
 
 ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  align-self: center;
+	flex: 1 1 0;
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+	align-self: center;
 }
 
 ul li {
-  --c: goldenrod;
-  color: var(--c);
-  font-size: 16px;
-  border: 0.3em solid var(--c);
-  border-radius: 0.5em;
-  width: 12em;
-  height: 3em;
-  text-transform: uppercase;
-  font-weight: bold;
-  font-family: sans-serif;
-  letter-spacing: 0.1em;
-  text-align: center;
-  line-height: 3em;
-  position: relative;
-  overflow: hidden;
-  z-index: 1;
-  transition: 0.5s;
-  margin: 1em;
-  background-color: white;
+	--c: goldenrod;
+	color: var(--c);
+	font-size: 2vh;
+	border: 0.5vh solid var(--c);
+	border-radius: 0.5vh;
+	width: 20vh;
+	height: 5.5vh;
+	text-transform: uppercase;
+	font-weight: bold;
+	font-family: sans-serif;
+	letter-spacing: 0.5vh;
+	text-align: center;
+	line-height: 5.5vh;
+	position: relative;
+	overflow: hidden;
+	z-index: 1;
+	transition: 0.5s;
+	margin: 1.5vh;
+	background-color: white;
 }
 
 ul li span {
-  position: absolute;
-  width: 25%;
-  height: 100%;
-  background-color: var(--c);
-  transform: translateY(150%);
-  border-radius: 50%;
-  left: calc((var(--n) - 1) * 25%);
-  transition: 0.5s;
-  transition-delay: calc((var(--n) - 1) * 0.1s);
-  z-index: -1;
+	position: absolute;
+	width: 25%;
+	height: 100%;
+	background-color: var(--c);
+	transform: translateY(150%);
+	border-radius: 50%;
+	left: calc((var(--n) - 1) * 25%);
+	transition: 0.5s;
+	transition-delay: calc((var(--n) - 1) * 0.1s);
+	z-index: -1;
 }
 
 ul li:hover {
-  color: white;
+	color: white;
 }
 
 ul li:hover span {
-  transform: translateY(0) scale(2);
+	transform: translateY(0) scale(2);
 }
 
 ul li span:nth-child(1) {
-  --n: 1;
+	--n: 1;
 }
 
 ul li span:nth-child(2) {
-  --n: 2;
+	--n: 2;
 }
 
 ul li span:nth-child(3) {
-  --n: 3;
+	--n: 3;
 }
 
 ul li span:nth-child(4) {
-  --n: 4;
+	--n: 4;
 }
 
 @media (max-width: 768px) {
-  section {
-    width: 100%;
-  }
+	section {
+		width: 100%;
+	}
 }
 </style>
